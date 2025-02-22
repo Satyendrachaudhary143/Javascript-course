fd9b0c75a4e5c36fbaef9fe99057b0b4031f2373
satyendra_chaudhary <satyendrachaudhary38@gmail.com>
2025-02-22 06:17:20 +0530

    data type

---
+++ 1_Basics level/2_variable.js
@@ -0,0 +1,11 @@
+console.log("this external file is linked to the html file");
+var Firstname = "John"; // var is a keyword to declare a variable and name is the variable name
+// var 5ram= 3; // this will throw an error as variable name cannot start with a number
+var $ram = 3; // this is a valid variable name
+var _ram = 3; // this is a valid variable name
+var ram5 = 3; // this is a valid variable name
+var ram = 3; // this is a valid variable name
+var RAM = 3; // this is a valid variable name
+// var first name= "John"; // this will throw an error as variable name cannot have space
+console.log(Firstname); // this will print the value of the variable in the console
+
---
+++ 1_Basics level/3_dataType.js
@@ -0,0 +1,16 @@
+// data type in javascript
+// primitive data type
+// 1. Number
+// 2. String
+// 3. Boolean
+// 4. Undefined
+// 5. Null
+// 6. Symbol
+// 7. BigInt
+// object data type
+// 1. Object
+// 2. Array
+// 3. Function
+// 4. Date
+// 5. RegExp
+// 6. Error
